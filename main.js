0116aa2ef7567b4be114155958f3b17c50bfb25d
ArthurKovalski <arthurkovalski987@gmail.com>
2022-02-23 19:02:45 -0500

    feat: task

--- main.js
+++ main.js
@@ -3,13 +3,33 @@ let x = ['bad','bad','good','good','good'];
 function well(x){
 let a = x.filter((item) => item === 'good').length;
  if(a >= 1 && a <= 2){
-     console.log( 'Publish!')
+     return 'Publish!'
  }else if( a > 2){
-     console.log( 'I smell a series')
+     return 'I smell a series'
  }else{
-     console.log( 'Fail!')
+     return 'Fail!'
  }
 } 
   well(x);
 
+//   Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers. 0 is neither positive nor negative.
 
+//   If the input is an empty array or is null, return an empty array.
+
+let a = [3,4,5,76,87,7,6,-7,-6,-5,-3,-2];
+
+function countPositivesSumNegatives(input) {
+ let arrContainer = [0,0];
+ if(input === null && input.length === 0){
+     return console.log([]);
+ }
+ for(let i = 0; i < input.length; i++){
+     if(input[i] <= 0){
+         arrContainer[1] += input[i]
+     }else if(input[i] > 0){
+         arrContainer[0] += 1
+     }
+ }
+ return console.log(arrContainer);
+}
+countPositivesSumNegatives(a)
